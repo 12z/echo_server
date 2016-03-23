ddc18e89d717a32281cca4c00f9e9352ab223ba1
Ivan Puzankov <12zrus@gmail.com>
2016-03-23 03:55:30 +0000

    comments added

--- client.c
+++ client.c
@@ -10,36 +10,37 @@
 #define BUFFER_SIZE 255
 
 int main(int argc, char *argv[]) {
-    int sockfd;
-    ssize_t m_size;
-    struct sockaddr_in server_addr;
-    struct hostent *server;
-    socklen_t server_len;
-    char buffer[BUFFER_SIZE + 1];
+    int sockfd; // file descriptor of socket
+    ssize_t m_size; // size of message
+    struct sockaddr_in server_addr; // address of a server socket
+    struct hostent *server; // hostname and address of a server
+    socklen_t server_len; // length of address
+    char buffer[BUFFER_SIZE + 1]; // duffer for message
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // create UDP socket
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(EXIT_FAILURE);
     }
-    server = gethostbyname("localhost");
+    server = gethostbyname("localhost"); // get address of a server
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
+    memset(&server_addr, 0, sizeof(server_addr)); // clear the structure
+    server_addr.sin_family = AF_INET; // IPv4
+    // fill address in server_addr structure
     memcpy((char *) &server_addr.sin_addr.s_addr, server->h_addr, (size_t) server->h_length);
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(PORT); // port that server listens
 
     while (1) {
 
-        /* get a message from the user */
+        // get a message from the user
         memset(buffer, 0, BUFFER_SIZE);
         printf("Please enter msg: ");
         fgets(buffer, BUFFER_SIZE, stdin);
 
-        /* send the message to the server */
+        // send the message to the server
         server_len = sizeof(server_addr);
         m_size = sendto(sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *) &server_addr, server_len);
         if (m_size < 0) {
@@ -47,12 +48,13 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        /* print the server's reply */
+        // get server's reply
         m_size = recvfrom(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &server_addr, &server_len);
         if (m_size < 0) {
             perror("ERROR in recvfrom");
             exit(EXIT_FAILURE);
         }
+        // print the server's reply
         printf("Echo from server: %s", buffer);
 
     }
--- server.c
+++ server.c
@@ -11,19 +11,20 @@
 #define BUFFER_SIZE 255
 
 int main(void) {
-    fd_set rfds;
-    int retval;
-    char buffer[BUFFER_SIZE + 1];
-    ssize_t m_size;//message size
-
-    int sockfd;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in client_addr;
+    fd_set rfds; // set of file descriptors to watch
+    int retval; // return value by select function
+    char buffer[BUFFER_SIZE + 1]; // buffer for messages
+    ssize_t m_size; // message size
+    int sockfd; // socket file descriptor
+    struct sockaddr_in serv_addr; // socket addres of the server
+    struct sockaddr_in client_addr; // socket address of the client from which message came
     socklen_t client_len; // length of client addr structure
-    struct hostent *hostp;
-    char *hostaddrp;
+    struct hostent *hostp; // hostname of a client from which message came
+    char *hostaddrp; // IP address of a client
 
-    // opening socket
+    client_len = sizeof(client_addr); // length of structure
+
+    // opening UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Error opening socket");
@@ -31,22 +32,20 @@ int main(void) {
     }
 
     // filling addr structure and binding socket to it
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
+    memset(&serv_addr, 0, sizeof(serv_addr)); // clear the structure
+    serv_addr.sin_family = AF_INET; // IPv4
+    serv_addr.sin_addr.s_addr = INADDR_ANY; // receive from any address
+    serv_addr.sin_port = htons(PORT); // port to listen
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error binding");
         exit(EXIT_FAILURE);
     }
 
-    client_len = sizeof(client_addr);
-
     while (1) {
-        FD_ZERO(&rfds);
-        FD_SET(sockfd, &rfds);
+        FD_ZERO(&rfds); // clears the set of file descriptors to watch
+        FD_SET(sockfd, &rfds); // add out socket to the list that should we watched
 
-        retval = select(sockfd + 1, &rfds, NULL, NULL, NULL);
+        retval = select(sockfd + 1, &rfds, NULL, NULL, NULL); // wait for data to arrive
 
         if (retval == -1) {
             perror("select()");
@@ -57,7 +56,7 @@ int main(void) {
         }
 
         if (FD_ISSET(sockfd, &rfds)) { // check that select returned our socket
-            memset(&buffer, 0, BUFFER_SIZE + 1);
+            memset(&buffer, 0, BUFFER_SIZE + 1); // clear the buffer
             m_size = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
                               (struct sockaddr *) &client_addr, &client_len); // get the message
             if (m_size < 0) {
@@ -65,20 +64,20 @@ int main(void) {
                 exit(EXIT_FAILURE);
             }
             hostp = gethostbyaddr((const char *) &client_addr.sin_addr.s_addr,
-                                  sizeof(client_addr.sin_addr.s_addr), AF_INET); // get the address of sender
+                                  sizeof(client_addr.sin_addr.s_addr), AF_INET); // get the address of a client
             if (hostp == NULL) {
                 perror("ERROR on gethostbyaddr");
                 exit(EXIT_FAILURE);
             }
-            hostaddrp = inet_ntoa(client_addr.sin_addr);
+            hostaddrp = inet_ntoa(client_addr.sin_addr); // get IP address of a client as a string
             if (hostaddrp == NULL) {
                 perror("ERROR on inet_ntoa\n");
                 exit(EXIT_FAILURE);
             }
             printf("server received datagram from %s (%s)\n",
-                   hostp->h_name, hostaddrp);
+                   hostp->h_name, hostaddrp); // print client data
             printf("server received %d/%d bytes: %s\n",
-                   (int) strlen(buffer), (int) m_size, buffer);
+                   (int) strlen(buffer), (int) m_size, buffer); // print message
 
 
             m_size = sendto(sockfd, buffer, strlen(buffer), 0,
